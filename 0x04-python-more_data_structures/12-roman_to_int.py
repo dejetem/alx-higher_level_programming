36b81104b98792fc7d34d606f6072328f98a870b
root <root@DESKTOP-K52IES9.localdomain>
2022-07-07 00:09:26 +0100

    update:2

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    if isinstance(roman_string, str):
+    if not isinstance(roman_string, str):
         return 0
     a = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
     b = 0
     c = 0
     for i in reversed(roman_string):
         b = a[i]
-        c += b if c < b * 5 else - b
+        c += b if c < b * 5 else -b
     return c
