367bb5234e7f98dbaeb58718ee5ac0421e29a741
root <root@DESKTOP-K52IES9.localdomain>
2022-07-07 00:40:52 +0100

    update:3.1

--- 0x04-python-more_data_structures/102-complex_delete.py
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -3,5 +3,5 @@ def complex_delete(a_dictionary, value):
     lastest = a_dictionary.copy()
     for a, b in lastest.items():
         if b == value:
-            del a_dictionary[k]
+            del a_dictionary[a]
     return a_dictionary
