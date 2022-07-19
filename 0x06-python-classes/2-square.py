a6c1a8e7dd8d8f359ece7c6b8494ebff7a79df5e
root <root@DESKTOP-K52IES9.localdomain>
2022-07-19 17:44:09 +0100

    update:0

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,18 +1,20 @@
 #!/usr/bin/python3
-"""Class Square"""
+"""Square module."""
 
 
 class Square:
-
-    """Private instance attribute: size
-    size must be an integer
-    The size is greater than or equal to 0
-    """
+    """Defines a square."""
 
     def __init__(self, size=0):
+        """Constructor.
+        Args:
+            size: Length of a side of the square.
+        Raises:
+            TypeError: If size is not an integer
+            ValueError: If size is less than 0
+        """
         if not isinstance(size, int):
             raise TypeError('size must be an integer')
-        elif size < 0:
+        if size < 0:
             raise ValueError('size must be >= 0')
-        else:
-            self.__size =
+        self.__size = size
