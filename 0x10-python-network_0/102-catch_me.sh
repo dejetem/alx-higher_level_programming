4eae99e040d3287d45b53554f964cf5fe8c95e4d
root <root@DESKTOP-K52IES9.localdomain>
2022-10-20 21:13:15 +0100

    update

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# catch as catch can
-curl -sL -X PUT -H "Origin:AlxSchool" -d "user_id=98" "0.0.0.0:5000/catch_me"
+# Catch me if you can!
+curl -sL -X PUT -H "Origin:HolbertonSchool" -d "user_id=98" "0.0.0.0:5000/catch_me"
