322ec41952786edacdf92b0acfc251c09343e703
root <root@DESKTOP-K52IES9.localdomain>
2022-11-15 20:32:56 +0100

    js api

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -2,7 +2,7 @@
 // script that display the status code of a GET request
 
 const request = require('request');
-request(process.argv[3], function (error, response, body) {
+request(process.argv[2], function (error, response, body) {
   error && console.log(error);
   response && console.log('code:', response.statusCode);
 });
